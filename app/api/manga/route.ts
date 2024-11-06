885055c2af6b262fa4ec047c8a50a59bb82b003d
Wildan Ardiansyah <wild4nard@gmail.com>
2024-11-06 07:53:09 +0700

    fixrwr

--- app/api/manga/route.ts
+++ app/api/manga/route.ts
@@ -1,5 +1,6 @@
-import { PrismaClient, Manga } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import type { Manga } from "@prisma/client";
 const prisma = new PrismaClient()
 
 export const POST = async (request:Request)=>{
